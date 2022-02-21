c41df5db3c2d590325351b6ac883c1e9ce955d1f
OlegMilkin <olegek.milkin@gmail.com>
2022-02-21 21:05:24 +0200

    Counter

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>+</button>
+      <span>0</span>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default Counter;
